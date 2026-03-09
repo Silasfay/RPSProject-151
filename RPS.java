d55af8798202380254a5f46d07baca7a958862f1
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-09 00:26:34 -0700

    Update RPS.java

--- RPS.java
+++ RPS.java
@@ -2,6 +2,6 @@
 
 public class RPS {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("Welcome to blehhh Rock Paper Scisors Game!");
     }
 }
