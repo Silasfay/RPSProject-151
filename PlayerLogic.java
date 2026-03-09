21c7cea3ed42566f4114542585fea9b41a749754
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-09 14:45:20 -0700

    Update PlayerLogic.java

--- PlayerLogic.java
+++ PlayerLogic.java
@@ -1,5 +1,11 @@
 public class PlayerLogic {
     public void getChoice(PlayerObject player) {
+        System.out.println("Make your choice: ");
+        System.out.pruntln("_______________")
+        System.out.pruntln("1) Rock");
+        System.out.pruntln("2) Paper");
+        System.out.pruntln("3) Scissors")
+        System.out.pruntln("_______________")
         // Logic to get player's choice
         playerLogic(){
             
