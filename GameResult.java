0ccdb2c489df7157cc9cf6d16bba9bbf8480c52f
Sandra Mata <sandra.mata1901@gmail.com>
2026-03-08 20:29:38 -0700

    Defined GameResult class

--- GameResult.java
+++ GameResult.java
@@ -1,4 +1,23 @@
 
 public class GameResult {
-    
+    private int player1Wins; 
+    private int player2Wins;
+
+    public void displayWinner() {
+        if (player1Wins > player2Wins) {
+            System.out.println("Player 1 won the game!");
+        } else if (player2Wins > player1Wins) {
+            System.out.println("Player 2 won the game!");
+        } else {
+            System.out.println("Draw!");
+        }
+    }
+
+    public void updateScore(String player1Choice, String player2Choice, PlayerChoices playerChoices) {
+        if (playerChoices.getBeats(player1Choice)[0].equals(player2Choice)) {
+            player1Wins++;
+        } else if (playerChoices.getBeats(player2Choice)[0].equals(player1Choice)) {
+            player2Wins++;
+        }
+    }
 }
