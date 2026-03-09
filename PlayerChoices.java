fdd7bde405314d1bef65b959c3a53885627ab825
Sandra Mata <sandra.mata1901@gmail.com>
2026-03-08 20:10:06 -0700

    Defined PlayerChoices class

--- PlayerChoices.java
+++ PlayerChoices.java
@@ -1,3 +1,20 @@
+import java.util.HashMap; 
+
 public class PlayerChoices {
-    
+    private HashMap<String, String[]> beats;
+
+    public PlayerChoices() {
+        beats = new HashMap<>();
+        beats.put("rock", new String[]{"scissors"});
+        beats.put("paper", new String[]{"rock"});
+        beats.put("scissors", new String[]{"paper"});
+    }
+
+    public HashMap<String, String[]> getChoices() {
+        return beats;
+    }
+
+    public String[] getBeats(String choice) {
+        return beats.get(choice);
+    }
 }
