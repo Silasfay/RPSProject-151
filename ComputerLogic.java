8b528f66ecd84a12eeb3fea3222930f7c143bd96
Silas <waffleiron58@gmail.com>
2026-03-09 17:42:48 -0700

    Redo CompLogic

--- ComputerLogic.java
+++ ComputerLogic.java
@@ -1,12 +1,19 @@
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ComputerLogic implements PlayerLogic {
     private Random ran;
+    private HashMap<String, String[]> choice;
 
-    public ComputerLogic(){
+    public ComputerLogic(HashMap<String, String[]> choices){
         this.ran = new Random();
+        this.choice =choices;
     }
     @Override
     public String getString(){
-        return null;
+        List<String> keys = new ArrayList<>(choice.keySet());
+        return keys.get(ran.nextInt(keys.size()));
     }
 }
