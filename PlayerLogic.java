9bf1687545c1b4ab9aebe75b406cce4a30ce04c4
Sandra Mata <sandra.mata1901@gmail.com>
2026-03-09 19:06:33 -0700

    Updated PlayerLogic to interface, removed duplicate PlayerLogicTemplate

--- PlayerLogic.java
+++ PlayerLogic.java
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-public class PlayerLogic {
-    public void getChoice(PlayerObject player) {
-        playerLogic(){
-            
-        }
-    }
-=======
 public interface PlayerLogic {
     String getChoice();
->>>>>>> Stashed changes
 }
