e354417678286392b9eb41676f3874054f4e12cd
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-09 17:50:50 -0700

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -1,4 +1,19 @@
-
+import java.util.Scanner;
 public class GameLogic {
-    
+    Scanner scnr = new Scanner(System.in)
+    int count = 0;
+    while(count <= 20){
+        System.out.println("Make your choice: ");
+        System.out.pruntln("_______________")
+        System.out.pruntln("1) Rock");
+        System.out.pruntln("2) Paper");
+        System.out.pruntln("3) Scissors")
+        System.out.pruntln("_______________")
+        int selection = scnr.nextInt();
+        count++;
+        //insert call for computer/player 2 choice
+
+        //print results of game
+    }
+        
 }
