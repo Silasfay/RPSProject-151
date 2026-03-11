5234be3acba898f979823338164be367349e8701
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-11 15:09:46 -0700

    Delete InputValidator.java

--- InputValidator.java
+++
@@ -1,6 +0,0 @@
-
-public class InputValidator {
-    public static boolean isValidChoice(String input, PlayerChoices playerChoices) {
-        return playerChoices.getChoices().containsKey(input);
-    }
-}
