8ce0bba7b817534f50b06b0d07c2af3af6f37a7f
Silas <waffleiron58@gmail.com>
2026-03-10 18:26:27 -0700

    spawned GameManager in Main

--- RPS.java
+++ RPS.java
@@ -3,5 +3,7 @@
 public class RPS {
     public static void main(String[] args) {
         System.out.println("Welcome to blehhh Rock Paper Scisors Game!");
+        GameManager manager = new GameManager();
+        manager.start();
     }
 }
