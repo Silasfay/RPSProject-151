18eed865ba3256bf453edfbd855f412fece663f5
Sandra Mata <sandra.mata1901@gmail.com>
2026-03-11 14:23:16 -0700

    Created RPSTest.java

---
+++ RPSTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RPSTest {
+    // TODO: Add unit tests 
+}
