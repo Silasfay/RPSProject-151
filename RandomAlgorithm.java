5e05cd517e5a0b8871dda72e6625bb703759e2ef
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-22 22:46:33 -0700

    Update RandomAlgorithm.java

--- RandomAlgorithm.java
+++ RandomAlgorithm.java
@@ -1,6 +1,27 @@
+import java.util.*;
 public class RandomAlgorithm implements ComputerAlgorithm {
+    private Random ran;
+    private HashMap<String, String[]> choices;
+
+    public RandomAlgorithm(HashMap<String, String[]> choices) {
+        this.ran = new Random();
+        this.choices = choices;
+    }
     @Override
-    public void runAlgorithm() {
+    public String getChoice() {
+        List<String> keys = new ArrayList<>(choices.keySet());
+        return keys.get(ran.nextInt(keys.size()));
+    }
+
+    @Override
+    public void recordRound(String opponentChoice) {
+        // do nothing
+    }
+
+    @Override
+    public void saveData() {
+        // do nothing
+    }
 
     }
 }
