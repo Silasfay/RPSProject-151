d7bfc78d1f4a1c3bb2336729a8a266ca23207c8a
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-23 07:12:57 -0700

    added comand line options to main

--- RPS.java
+++ RPS.java
@@ -3,6 +3,19 @@
 public class RPS {
     public static void main(String[] args) {
         System.out.println("Welcome to blehhh Rock Paper Scisors Game!");
+
+        //comand line prompts
+        String mode = "PROMPT"; //default bahavior
+
+        for(String arg : args){
+            if(arg.equalsIgnoreCase("-r")){
+                mode = "RANDOM";
+            }
+            else if(arg.equalsIgnoreCase("-m")){
+                mode = "ML";
+            }
+        }
+        
         GameManager manager = new GameManager();
         manager.start();
     }
