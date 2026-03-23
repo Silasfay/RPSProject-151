5cec15784dfaa5bddac89e9945191db13c817379
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-22 17:09:39 -0700

    Update ComputerLogic.java

--- ComputerLogic.java
+++ ComputerLogic.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class ComputerLogic implements PlayerLogic {
     private Random ran;
