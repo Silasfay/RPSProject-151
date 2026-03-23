5cb930a2f3afd97977a7e1328da498b578355850
Silas <waffleiron58@gmail.com>
2026-03-23 12:21:28 -0700

    Removed logic added to Factory

--- GameSetUp.java
+++ GameSetUp.java
@@ -19,46 +19,20 @@ public class GameSetUp {
         return scr.nextLine();
     }
 
-    private PlayerLogic setPlayerType(String name){
-        while(true){
-            System.out.print("Is " + name + " Human, Random Computer, or Trained Computer? (H/RC/TC): ");
-            String input = scr.nextLine().trim().toUpperCase();
-            
-            // pass choices to HumanLogic so it can display the menu
-            if(input.equals("H")){
-                return new HumanLogic(scr, choices);
-              }
-            // if user chooses a computer, apply command-line mode if set
-            if(input.equals("RC") || input.equals("TC")){
-                if(computerMode.equals("RANDOM")){
-                    System.out.println("Command-line override: " + name + " will use RANDOM algorithm");
-                    return new ComputerLogic(new RandomAlgorithm(choices.getChoices()));
-                }
-                if(computerMode.equals("ML")){
-                    System.out.println("Command-line override: " + name + " will use MACHINE LEARNING algorithm");
-                    return new ComputerLogic(new MachineLearningAlgorithm(choices.getChoices()));
-                }else {
-                    // user selected type interactively
-                    if(input.equals("RC")){
-                        return new ComputerLogic(new RandomAlgorithm(choices.getChoices()));
-                    } else {
-                        return new ComputerLogic(new MachineLearningAlgorithm(choices.getChoices()));
-                    }
-                }
-            }
-            //if not H or RC or TC entered
-            System.out.println("Invalid input. Please enter H for Human, RC for Random Computer, or TC for Trained Computer");
-            
-        }
-    }
+// just asks, moved all logic to factory
+private String setPlayerType(String name){
+    System.out.print("Is " + name + " Human, Random Computer, or Trained Computer? (H/RC/TC): ");
+    return scr.nextLine().trim().toUpperCase();
+}
 
-    public ArrayList<PlayerObject> buildPlayers(){
-        ArrayList<PlayerObject> playerList = new ArrayList<>();
-        for(int i = 0; i < 2; i++){
-            String name = setPlayerName();
-            PlayerLogic logic = setPlayerType(name);
-            playerList.add(factory.createPlayer(name, logic));
-        }
-        return playerList;
+//passes string to factory
+public ArrayList<PlayerObject> buildPlayers(){
+    ArrayList<PlayerObject> playerList = new ArrayList<>();
+    for(int i = 0; i < 2; i++){
+        String name = setPlayerName();
+        String type = setPlayerType(name);
+        playerList.add(factory.createPlayer(name, type, computerMode));
     }
+    return playerList;
+}
 }
