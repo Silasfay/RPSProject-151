87c280cdb6648e82cca43b60a017b479b4f26556
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-22 22:49:28 -0700

    Update MachineLearningAlgorithm.java

--- MachineLearningAlgorithm.java
+++ MachineLearningAlgorithm.java
@@ -1,6 +1,70 @@
 public class MachineLearningAlgorithm implements ComputerAlgorithm {
-    @Override
-    public void runAlgorithm() {
+    private HashMap<String, Integer> frequencies;
+    private LinkedList<String> history;
+    private HashMap<String, String[]> choices;
+    private int N = 5;}
 
+    private TCDataStore datastore;
+
+    public MachineLearningAlgorithm(HashMap<String, String[]> choices) {
+        this.choices = choices;
+        this.datastore = new TCDataStore();
+        this.frequencies = datastore.load();
+        this.history = new LinkedList<>();
     }
+
+    @Override
+    public String getChoice() {
+
+        if (history.size() < N - 1) return randomChoice();
+
+        String lastSeq = String.join("", history);
+
+        int max = -1;
+        String predicted = null;
+
+        for (String seq : frequencies.keySet()) {
+            if (seq.startsWith(lastSeq)) {
+                int freq = frequencies.get(seq);
+                if (freq > max) {
+                    max = freq;
+                    predicted = seq.substring(seq.length() - 1);
+                }
+            }
+        }
+        if (predicted == null) return randomChoice();
+
+        for (String move : choices.keySet()) {
+            if (choices.get(move)[0].equals(predicted)) {
+                return move;
+            }
+        }
+
+        return randomChoice();
+    }
+
+    private String randomChoice() {
+        List<String> keys = new ArrayList<>(choices.keySet());
+        return keys.get(new Random().nextInt(keys.size()));
+    }
+
+    @Override
+    public void recordRound(String opponentChoice) {
+        history.add(opponentChoice);
+
+        if (history.size() > N) {
+            history.removeFirst();
+        }
+
+        if (history.size() == N) {
+            String seq = String.join("", history);
+            frequencies.put(seq, frequencies.getOrDefault(seq, 0) + 1);
+        }
+    }
+
+    @Override
+    public void saveData() {
+        datastore.save(frequencies);
+    }
+
 }
