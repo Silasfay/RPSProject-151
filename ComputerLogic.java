d1954152f31d711ea3f560e680e0bdc9fcfdd38d
Izy <43360734+plaidsweater@users.noreply.github.com>
2026-03-22 23:02:01 -0700

    Update ComputerLogic.java
    deleeted repeated stuff in computer ligic

--- ComputerLogic.java
+++ ComputerLogic.java
@@ -1,110 +1,23 @@
 import java.util.*;
 
-enum Algorithm {
-    RANDOM,
-    ML
-}
-
 public class ComputerLogic implements PlayerLogic {
-    private Random ran;
-    private HashMap<String, String[]> choice;
+    private ComputerAlgorithm algorithm;
 
-    // TODO: use enums in order to switch which ComputerAlgorithm to create for a player with ComputerLogic
-    protected ComputerAlgorithm computerAlgorithm;
-    Algorithm algorithmType = Algorithm.RANDOM;
-
-    private boolean isTC;
-    private HashMap<String, Integer> frequencies;
-    private LinkedList<String> history;
-    private int N = 5;
-
-    private TCDataStore datastore;
-
-    // TODO: Implement this further to avoid needing to use checkers for whether the object is TC or not
-    public ComputerLogic() {
-        switch (algorithmType) {
-            case Algorithm.RANDOM:
-                computerAlgorithm = new RandomAlgorithm();
-                break;
-            case Algorithm.ML:
-                computerAlgorithm = new MachineLearningAlgorithm();
-                break;
-        }
-    }
-
-    public ComputerLogic(HashMap<String, String[]> choices) {
-        this(choices, false);
-    }
-
-    public ComputerLogic(HashMap<String, String[]> choices){
-        this.ran = new Random();
-        this.choice = choices;
-        this.isTC = isTC;
-
-        if(isTC){
-            dataStore = new TCDataStore();
-            frequencies = dataStore.load(); //load saved data
-            history = new LinkedList<>();
-        }
-    }
-
-    // TODO: needs implementation in random and ML algorithm
-    public void performAlgorithm() {
-        computerAlgorithm.runAlgorithm();
+    public ComputerLogic(ComputerAlgorithm algorithm) {
+        this.algorithm = algorithm;
     }
 
     @Override
-    public String getChoice(){
-        if (!isML) return randomChoice();
-
-        if (history.size() < N - 1) return randomChoice();
-
-        String lastSeq = String.join("", history);
-
-        int max = -1;
-        String predictedHuman = null;
-
-        for (String seq : frequencies.keySet()) {
-            if (seq.startsWith(lastSeq)) {
-                int freq = frequencies.get(seq);
-                if (freq > max) {
-                    max = freq;
-                    predictedHuman = seq.substring(seq.length() - 1);
-                }
-            }
-        }
-        if(predictedHuman == null) return randomChoice();
-
-        for(String move : choices.keySet()){
-            if(choices.get(move)[0].equals(predictedHuman)){
-                return move;
-            }
-        }
-        return randomChoice();
+    public String getChoice() {
+        return algorithm.getChoice();
     }
 
-    private String randomChoice() {
-        List<String> keys = new ArrayList<>(choices.keySet());
-        return keys.get(ran.nextInt(keys.size()));
+    public void recordRound(String opponentChoice) {
+        algorithm.recordRound(opponentChoice);
     }
 
-    public void recordRound(String humanChoice){
-        if(!isTC) return;
-
-        history.add(humanChoice);
-        if(history.size() > N){
-            history.removeFirst();
-        }
-
-        if history.size() == N{
-            String seq = String.join("", history);
-            frequencies.put(seq, frequencies.getORDefault(seq, 0) + 1);
-        }
-    }
-    
     public void saveData() {
-        if (!isTC) return;
-        dataStore.save(frequencies);
+        algorithm.saveData();
     }
     
 }
